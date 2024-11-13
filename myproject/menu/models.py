5207102451d75f9bc0510dc8ae97949b07ef75fc
lizekss <lekse20@freeuni.edu.ge>
2024-11-13 23:03:58 +0400

    models setup

--- myproject/menu/models.py
+++ myproject/menu/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class MenuSubCategory(models.Model):
+    name = models.CharField(max_length=255)
+    cover_image = models.ImageField(upload_to='subcategory_covers/')
+    parent_category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name='subcategories')
+
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='dish_photos/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sub_category = models.ForeignKey(MenuSubCategory, on_delete=models.CASCADE, related_name='dishes')
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name='ingredients')
+
+    def __str__(self):
+        return self.name
--- myproject/restaurants/models.py
+++ myproject/restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    cover_photo = models.ImageField(upload_to='restaurant_covers/')
+
+    def __str__(self):
+        return self.name
