4d4f1ee0f25a3a24bd9fdf56b4efff87e9e9335c
lizekss <lekse20@freeuni.edu.ge>
2024-11-14 19:16:33 +0400

    add authentication

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurants/', include('restaurants.urls')),
     path('menu/', include('menu.urls')),
+    path('user/', include('user.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
---
+++ user/views.py
@@ -0,0 +1,6 @@
+from rest_framework import generics
+from .serializers import UserRegistrationSerializer
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
